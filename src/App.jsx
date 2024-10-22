215e372f1e964e19ef360ffae70950113e9358f2
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-23 02:12:00 +0600

    handle event write system in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick() {
+    alert('Button Clicked');
+  }
+  const handleClick2 = () => {
+    alert("Button 2 Clicked")
+  }
 
+  const addToFive = (num) =>  {
+    alert(num+5)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>React Core Concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click2</button>
+      <button onClick={() => { alert('Button 3 Clicked') }}>Click3</button>
+      <button onClick={() => addToFive(3)}>Click 4</button>
     </>
   )
 }
